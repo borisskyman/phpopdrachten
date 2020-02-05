6af15f743f3bea29fbd06917253237f72b7ccc1d
Boris Vasic <boris.vasic@edu-kw1c.nl>
2020-02-05 12:16:05 +0100

    Test Test

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<?php/**
+ * User: Boris Vasic
+ * Date: 5-2-2020
+ * Time: 12:04 AM
+ * File: index.php
+ */?>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <title><?php print "pagina 1"; ?></title>
+    </head>
+    <body>
+    <h1>
+        <?php
+        echo "Hello world";
+        ?>
+    </h1>
+    <header>
+        <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+    </header>
+    <aside>
+        <h2>Menu</h2>
+        <ul>
+            <li>Hoofdstuk 2
+                <ul>
+                    <li>
+                        <a href="hoofdstuk2/opdracht_2-1.php">Opdracht 2.1</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </aside>
+    <main id="wrapper">
+        <h2>Uitwerkingen</h2>
+    </main>
+    </body>
+</html>
