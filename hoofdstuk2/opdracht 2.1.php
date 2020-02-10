a0c18730033b41dfd2b913088ccd8f947310f2b9
Boris Vasic <boris.vasic@edu-kw1c.nl>
2020-02-10 13:40:03 +0100

    Hoofdstuk 2 map met Opdracht 2.1 toegevoegd, css map met style.css toegevoegd.

---
+++ hoofdstuk2/opdracht 2.1.php
@@ -0,0 +1,20 @@
+<?php/**
+ * User: Boris Vasic
+ * Date: 5-2-2020
+ * Time: 12:04 AM
+ * File: index.php
+ */?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <title><?php print "Opdracht 2.1"; ?></title>
+    </head>
+    <body>
+        <h1>
+            <?php
+            echo "Hello world";
+            ?>
+        </h1>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,21 +1,16 @@
-<!DOCTYPE html>
 <?php/**
  * User: Boris Vasic
  * Date: 5-2-2020
  * Time: 12:04 AM
  * File: index.php
  */?>
+<!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8"/>
         <title><?php print "pagina 1"; ?></title>
     </head>
     <body>
-    <h1>
-        <?php
-        echo "Hello world";
-        ?>
-    </h1>
     <header>
         <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
     </header>
@@ -25,7 +20,7 @@
             <li>Hoofdstuk 2
                 <ul>
                     <li>
-                        <a href="hoofdstuk2/opdracht_2-1.php">Opdracht 2.1</a>
+                        <a href="hoofdstuk2/opdracht%202.1.php">Opdracht 2.1</a>
                     </li>
                     <li>
                         <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
