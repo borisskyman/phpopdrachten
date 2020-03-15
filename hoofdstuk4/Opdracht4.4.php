aba6f5b33adf786ba18c8cfa614882e2f95d452e
Boris Vasic <boris.vasic@edu-kw1c.nl>
2020-03-15 22:59:03 +0100

    Oplevering opdracht 4.4

--- hoofdstuk4/Opdracht4.4.php
+++ hoofdstuk4/Opdracht4.4.php
@@ -7,28 +7,28 @@
  */
 $opdracht = "Opdracht 4.4";
 include '../Includes/header.php'; ?>
-<body>
-<h1>
-    Weekkalender komende week:
-</h1>
-<?php
+    <body>
+        <h1>
+            Weekkalender komende week:
+        </h1>
+        <?php
 
-$dagNamen = array("zondag","maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag");
-for ($day = 0; $day <7 ; $day++)
-{
-    $datumAlsGetal = strtotime("+$day days");
-    echo "<br>";
-    echo "Dag ";
-    echo date('w', $datumAlsGetal);
-    echo " is ";
-    echo "$dagNamen[$day] ";
-    echo date('d-m-Y', $datumAlsGetal);
-}
-?>
-<br>
-<br>
-<br>
-<a href="../index.php">Terug</a>
-<?php include '../Includes/footer.php'; ?>
-</body>
+        $dagNamen = array("zondag","maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag", "zondag");
+        for ($day = 0; $day <8 ; $day++)
+        {
+            $datumAlsGetal = strtotime("+$day days");
+            echo "<br>";
+            echo "Dag ";
+            echo date('w', $datumAlsGetal);
+            echo " is ";
+            echo "$dagNamen[$day] ";
+            echo date('d-m-Y', $datumAlsGetal);
+        }
+        ?>
+        <br>
+        <br>
+        <br>
+        <a href="../index.php">Terug</a>
+        <?php include '../Includes/footer.php'; ?>
+    </body>
 </html>
