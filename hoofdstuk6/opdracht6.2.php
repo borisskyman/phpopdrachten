63b556e0aaf7f6a575ecc79ef6dfbb5ee2cd1b7f
Boris Vasic <boris.vasic@edu-kw1c.nl>
2020-05-06 00:00:12 +0200

    Taak 1 van 6.2

--- hoofdstuk6/opdracht6.2.php
+++ hoofdstuk6/opdracht6.2.php
@@ -14,12 +14,17 @@ include '../Includes/header.php'; ?>
 <p>
     Speel het spel steen - papier - schaar tegen de computer
 </p>
-<input type="radio" id="steen" name="steen" value="steen">
+<form action="checkGame.php" method="post">
+<input type="radio" id="steen" name="steen" value="steen" onchange="document.getElementById('gameFrm').submit();">
 <img src="images/Rock.jpg" id="Rock">
-<input type="radio" id="papier" name="papier" value="papier">
+<input type="radio" id="papier" name="papier" value="papier" onchange="document.getElementById('gameFrm').submit();">
 <img src="images/Paper.png" id="Paper">
-<input type="radio" id="schaar" name="schaar" value="schaar">
+<input type="radio" id="schaar" name="schaar" value="schaar" onchange="document.getElementById('gameFrm').submit();">
 <img src="images/Scissors.png" id="Scissors">
+</form>
+<br>
+<br>
+<?php include 'checkGame.php' ?>
 <br>
 <a href="../index.php">Terug</a>
 <?php include '../Includes/footer.php'; ?>
