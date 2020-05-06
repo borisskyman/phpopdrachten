a7a8446b2bf1382d59f526c6f93ee8553d4903cf
Boris Vasic <boris.vasic@edu-kw1c.nl>
2020-05-06 07:51:19 +0200

    Taak 4 van 6.2

--- hoofdstuk6/opdracht6.2.php
+++ hoofdstuk6/opdracht6.2.php
@@ -17,25 +17,61 @@ include '../Includes/header.php'; ?>
 <form id="gameFrm" method="get" action="opdracht6.2.php">
 <div class="float">
     <input type="radio" onchange="document.getElementById('gameFrm').submit();" name="keuze" value="steen">
-    <img src="images/steen.png" id="Rock">
+    <img src="images/steen.png" class='SteenPapierSchaar'>
     </div>
 <div class="float">
     <input type="radio" onchange="document.getElementById('gameFrm').submit();" name="keuze" value="papier">
-    <img src="images/papier.png" id="Paper">
+    <img src="images/papier.png" class='SteenPapierSchaar'>
 </div>
 <div class="float">
     <input type="radio" onchange="document.getElementById('gameFrm').submit();" name="keuze" value="schaar">
-    <img src="images/schaar.png" id="Scissors">
+    <img src="images/schaar.png" class='SteenPapierSchaar'>
 </div>
 <br>
 <div>
+<p>
+    Start het spel door schaar, papier of steen te kiezen. De computer kiest samen tegelijkertijd met jou.
+</p>
 <?php
-    // Wat kiest de computer
-    //Random wordt er een getal tussen de 0 en 2 gekozen
+    if (isset($_GET['keuze']))
+    {
+        echo "Jij koos: <img src='images/{$_GET['keuze']}.png' class='SteenPapierSchaar'>";
+        // Wat kiest de computer
+        //Random wordt er een getal tussen de 0 en 2 gekozen
         $opties = array("steen","papier","schaar");
         $computerkeuzegetal = rand(0,2);
         $computerkeuze = $opties[$computerkeuzegetal];
-        echo "&nbsp;&nbsp;De computer koos: <img src='images/{$computerkeuze}.png' id='KeuzePapierSteenOfSchaar'>";
+        echo "&nbsp;&nbsp;De computer koos: <img src='images/{$computerkeuze}.png' class='SteenPapierSchaar'> <br><br>";
+        $spelerKeuze = $_GET['keuze'];
+        if ($spelerKeuze == $computerkeuze)
+        {
+            echo "gelijke keuzes wint niemand";
+        }
+        else if ($spelerKeuze == "schaar" && $computerkeuze == "papier")
+        {
+            echo "schaar wint van papier";
+        }
+        else if ($spelerKeuze == "schaar" && $computerkeuze == "steen")
+        {
+            echo "schaar verliest van steen";
+        }
+        else if ($spelerKeuze == "steen" && $computerkeuze == "schaar")
+        {
+            echo "steen wint van schaar";
+        }
+        else if ($spelerKeuze == "steen" && $computerkeuze == "papier")
+        {
+            echo "steen verliest van papier";
+        }
+        else if ($spelerKeuze == "papier" && $computerkeuze == "steen")
+        {
+            echo "papier wint van steen";
+        }
+        else if ($spelerKeuze == "papier" && $computerkeuze == "schaar")
+        {
+            echo "papier verliest van schaar";
+        }
+    }
 ?>
 </div>
 </form>
