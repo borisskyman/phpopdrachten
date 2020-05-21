8998425a9a7cfeba3de5749b93e655ad99d3df7f
Boris Vasic <boris.vasic@edu-kw1c.nl>
2020-05-22 00:03:51 +0200

    Opdracht 7.3.php
     Ik moet alleen nog de zoekfunctie bijvoegen
    
     Nu werkt het niet en geeft het een foutmelding aan:
     PHP Fatal error:  Uncaught Error: Call to a member function fetch() on bool in C:\inetpub\wwwroot\phpopdrachten\hoofdstuk7\Opdracht7.3.php:45
     Stack trace:
     #0 {main}
       thrown in C:\inetpub\wwwroot\phpopdrachten\hoofdstuk7\Opdracht7.3.php on line 45

--- hoofdstuk7/Opdracht7.3.php
+++ hoofdstuk7/Opdracht7.3.php
@@ -2,63 +2,62 @@
 /**
  * User: Boris Vasic
  * Date: 21-5-2020
- * Time: 22:27
+ * Time: 23:41
  * File: opdracht7.3.php
  */
+
 $opdracht = "Opdracht 7.3";
-include '../Includes/header.php'; ?>
-<body>
+include '../Includes/header.php';
+
+// Inladen functies bestand
+include("functions.php");
+
+// Lege Array aanmaken voor de results
+$aJokes = array();
+
+// Starten van een database connectie
+startConnection();
+
+// Executeren van een SQL query
+if (isset($_GET['jokesearch']))
+{
+    $SearchedJoke = $_GET['jokesearch'];
+    $query = 'SELECT * FROM joke WHERE joketext LIKE %'.$SearchedJoke.'%';
+}
+else
+{
+    $query = "SELECT * FROM joke";
+}
+$jokes = executeQuery($query);
+
+echo "<p> $query </p>";
+?>
+<form method="get">
+    <label>
+        Zoekterm:
+    </label>
+    <input type="text" id="jokesearch" name="jokesearch">
+    <input type="submit" value="Zoeken">
+</form>
+<hr>
+<?php
+// Resultaten rij voor rij ophalen
+while($row = $jokes->fetch(PDO::FETCH_ASSOC) )
+{
+    $aJokes[] = $row;
+}
+
+?>
 <?php
 
-    try //de code in het try block is de enige code die wordt uitgevoerd
-        // als deze code succesvol wordt uitgevoerd
-    {
-        $pdo = new PDO("odbc:odbc2sqlserver");
-    }
-        // de code in het catch blok wordt alleen uitgevoerd als er een fout
-        // optreedt in de code in het try block
-    catch (PDOException $e)
-    {
-        echo "<h1>Database Error:</h1>";
-        echo $e->getMessage();
-        die();
-    }
-    //Het resultaat van bovenstaande code is dat ik een variabele $pdo heb
-    // van het type object, waarmee ik een query kan uitvoeren op de database
-
-    // Lege Array aanmaken voor de results
-    $aJokes = array();
-    echo "database connectie gelukt";
-
-    // $sql is de sql statement die ik wil uitvoeren op de database,
-    // en aangezien $pdo de connectie-handler is (het object dat de connectie vasthoudt)
-    // moet ik de query wel op die manier uitvoeren
-
-    // $sql = 'SELECT * FROM joke WHERE jokedate < GETDATE();';
-    $sql = 'SELECT * FROM joke';
-    $result = $pdo->query($sql);
-    while ($row = $result->fetch(PDO::FETCH_ASSOC))
-    {
-        // omdat row een associatieve array is kan ik de waardes als associaties
-        // uit de rij halen, dus:
-        $aJokes[] = $row;
-    }
-    //print_r($aJokes);
-    //var_dump($aJokes);
-
-    // Genereert een HTML tabel met de waardes van een SQL database genaamd ijdb.
-    // Die jokes laat zien, de id, het begin, de mop, en de jokedatum
-    echo "<br><br><table>";
-    echo "<tr><td class='JokeCSS'>ID</td><td class='JokeCSS'>Joketext</td><td class='JokeCSS'>Jokeclou</td><td class='JokeCSS'>Jokedate</td></tr>";
-    foreach($aJokes as $grap)
-    {
-        echo "<tr><td>".$grap['id']."</td><td>".$grap['joketext']."</td><td>".$grap['jokeclou']."</td><td>".$grap['jokedate']."</td></tr>";
-    }
-    echo "</table>";
-
+echo "<table class='JokeSearchCSS'>";
+echo "<tr><th class='JokeSearchCSS'>Jokeid</th><th class='JokeSearchCSS'>Joketext</th><th class='JokeSearchCSS'>Jokeclou</th><th class='JokeSearchCSS'>Jokedate</th></tr>";
+foreach($aJokes as $grap)
+{
+    echo "<tr><td class='jokeData'>".$grap['id']."</td><td class='jokeData'>".$grap['joketext']."</td><td class='jokeData'>".$grap['jokeclou']."</td><td class='jokeData'>".$grap['jokedate']."</td></tr>";
+}
+echo "</table>";
 ?>
 <br>
 <a href="../index.php">Terug</a>
 <?php include '../Includes/footer.php'; ?>
-</body>
-</html>
